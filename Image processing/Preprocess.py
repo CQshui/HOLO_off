7d48be51aebda83fe77d6e7b0f3e9958fcefe312
CQshui <1009893597@qq.com>
2024-02-28 19:21:20 +0800

    完善二值化部分

--- Image processing/Preprocess.py
+++ Image processing/Preprocess.py
@@ -3,12 +3,12 @@ import numpy as np
 
 # pix = 0.098e-6  # unit: m
 pix = 0.098  # unit: um
-img_path = 'D:\\Desktop\\test\\offaxis\\Gypsum\\test.jpg'
+img_path = 'D:\\Desktop\\test\\dof\\DOF.bmp'
 img = cv2.imread(img_path, 0)
 im_height, im_width = img.shape
 # _, binary_image = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
 # ret2, binary_image = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-binary_image = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 1505, 50)
+binary_image = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 2005, 25)
 inverted_image = cv2.bitwise_not(binary_image)
 cv2.imwrite('D:\\Desktop\\test\\offaxis\\Gypsum\\result\\binary.jpg', inverted_image)
 # 图片边缘平滑化，考虑15个像素
@@ -17,9 +17,9 @@ inverted_image[0:im_height-15, im_width-15:im_width] = 0
 inverted_image[im_height-15:im_height, 15:im_width] = 0
 inverted_image[15:im_height, 0:15] = 0
 # 开运算&闭运算
-kernel1 = np.ones((15, 15), np.uint8)
+kernel1 = np.ones((30, 30), np.uint8)
 opened = cv2.morphologyEx(inverted_image, cv2.MORPH_OPEN, kernel1)
-kernel2 = np.ones((5, 5), np.uint8)
+kernel2 = np.ones((30, 30), np.uint8)
 closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel2)
 # 转变为三通道图片
 bgr_image = cv2.cvtColor(opened, cv2.COLOR_GRAY2BGR)
