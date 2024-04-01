ed318d73f6c63c2f83e0fac67ca86eb538ddcc51
CQshui <1009893597@qq.com>
2024-04-01 13:51:34 +0800

    中值滤波去背景

--- Data Generation/fig_generate.py
+++ Data Generation/fig_generate.py
@@ -6,13 +6,18 @@ import numpy as np
 
 def generate(img_path, save_path, choice, types, file_number=0):
     pix = 0.098
-    # img_path = 'C:\\Users\\d1009\\Desktop\\generation\\input\\Gypsum\\DOF_gypsum.bmp'
     img = cv2.imread(img_path, 0)
     print(img_path)
     im_height, im_width = img.shape
-    binary_image = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
-                                         805, 5)
-    inverted_image = cv2.bitwise_not(binary_image)
+    # binary_image = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,
+    #                                      805, 5)
+    # inverted_image = cv2.bitwise_not(binary_image)
+
+    # 中值滤波
+    img_mida = cv2.medianBlur(img, 201)
+    img_sub = cv2.addWeighted(img, -1, img_mida, 1, 0)
+    _, thresh = cv2.threshold(img_sub, 15, 255, cv2.THRESH_BINARY)
+    inverted_image = thresh.copy()
     # 图片边缘规整，考虑edge个像素
     edge = 50
     inverted_image[0:edge, 0:im_width-edge] = 0
@@ -20,15 +25,14 @@ def generate(img_path, save_path, choice, types, file_number=0):
     inverted_image[im_height-edge:im_height, edge:im_width] = 0
     inverted_image[edge:im_height, 0:edge] = 0
     # 开运算&闭运算
-    kernel1 = np.ones((5, 5), np.uint8)
+    kernel1 = np.ones((11, 11), np.uint8)
     opened = cv2.morphologyEx(inverted_image, cv2.MORPH_OPEN, kernel1)
-    kernel2 = np.ones((15, 15), np.uint8)
+    kernel2 = np.ones((55, 55), np.uint8)
     closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel2)
     # 转变为三通道图片
-    bgr_image = cv2.cvtColor(opened, cv2.COLOR_GRAY2BGR)
-    rgb_image = bgr_image[:, :, ::-1]
+    bgr_image = cv2.cvtColor(closed, cv2.COLOR_GRAY2BGR)
     # 计算连通域数量，框选计数
-    contours, _ = cv2.findContours(opened, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     count = 0
     # 画出轮廓图
     contour_img = np.zeros_like(img)
@@ -69,13 +73,21 @@ def generate(img_path, save_path, choice, types, file_number=0):
 
 
 if __name__ == '__main__':
-    im_path = 'F:/20240326/Limestone'
-    saving_path = 'F:/20240326/Single/'
+
+    im_path = 'F:/Data/20240329/DOF/Limestone'
+    saving_path = 'F:/Data/20240329/Single/'
     images = os.listdir(im_path)
     image_files = [f for f in images if any(ext in f.lower() for ext in ('.jpg', '.jpeg', '.png', '.bmp'))]
     number = 0
     for image in image_files:
-        im_path = 'F:/20240326/Limestone'
+        im_path = 'F:/Data/20240329/DOF/Limestone'
         number += 1
         im_path = im_path + '/' + image
-        generate(im_path, saving_path, 'origin', 'Limestone', number)
+        try:
+            generate(im_path, saving_path, 'origin', 'Limestone', number)
+
+        except Exception as e:
+            print(e)
+            pass
+
+
